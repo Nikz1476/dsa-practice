542ea835d3483bd7ec2094e53bf7f96617ccae0a
Nikz1476 <nikhil.a1476@gmail.com>
2026-03-12 15:56:38 +0530

    Time: 35 ms (39.42%) | Memory: 13.5 MB (89.60%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -4,6 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # #brute force time - O(n^2), space = O(1)
+        # n = len(nums)
+        # max_count = 0
+
+        # for i in range(n):
+        #     count = 0
+        #     for j in range(i, n):
+        #         if nums[j] == 1:
+        #             count += 1
+        #             max_count = max(max_count, count)
+        #         else:
+        #             break
+
+        # return max_count
+
+        #optimal - using running counter 
         count = 0
         max_count = 0
 
@@ -15,3 +31,16 @@ class Solution(object):
                 count = 0
 
         return max_count
+
+        #both time and space complexity O(n) and O(1)
+
+        # left = 0
+        # max_len = 0
+
+        # for right in range(len(nums)):
+        #     if nums[right] == 0:
+        #         left = right + 1
+        #     else:
+        #         max_len = max(max_len, right - left + 1)
+
+        # return max_len
