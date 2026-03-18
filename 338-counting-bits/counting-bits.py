2f98bd70d022584a54ef93828d19486340c1b9b1
Nikz1476 <nikhil.a1476@gmail.com>
2026-03-18 22:47:49 +0530

    Time: 3 ms (99.55%) | Memory: 17.7 MB (74.05%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        # # brute force, time - O(n log n), space - O(1)
+        # def count_ones(x):
+        #     count = 0
+        #     while x:
+        #         x = x & (x - 1)
+        #         count += 1
+        #     return count
+
+        # return [count_ones(i) for i in range(n + 1)]
+
+        # # optimal, time - O(n), space - O(1)
+        ans = [0] * (n + 1)    
+        # for i in range(1, n + 1): right shift and removes the last bit
+        #     ans[i] = ans[i >> 1] + (i & 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i & (i - 1)] + 1
+    
+        return ans
