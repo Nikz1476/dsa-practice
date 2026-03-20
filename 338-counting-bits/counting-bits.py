481be57c259db162c2f0ef50ce8fd18f16e4a06b
Nikz1476 <nikhil.a1476@gmail.com>
2026-03-20 19:12:34 +0530

    Time: 7 ms (76.65%) | Memory: 17.8 MB (73.44%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -14,10 +14,11 @@ class Solution(object):
 
         # return [count_ones(i) for i in range(n + 1)]
 
-        # # optimal, time - O(n), space - O(1)
+        # # optimal, time - O(n), space - O(n)
         ans = [0] * (n + 1)    
         # for i in range(1, n + 1): right shift and removes the last bit
         #     ans[i] = ans[i >> 1] + (i & 1)
+        #     ans[i] = ans[i // 2] + (i % 2)
         for i in range(1, n + 1):
             ans[i] = ans[i & (i - 1)] + 1
     
