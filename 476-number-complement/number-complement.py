58c1fff3701b9b2c2befc863c787e3ce2b232b67
Nikz1476 <nikhil.a1476@gmail.com>
2026-03-20 19:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (72.33%) - LeetSync

---
+++ 476-number-complement/number-complement.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        # # brute force, time - O(log n)^2, space - O(logn n)
+        # binary = bin(num)[2:]
+        # flipped = ""
+        # for bit in binary:
+        #     if bit == '0':
+        #         flipped += '1'
+        #     else:
+        #         flipped += '0'
+    
+        # return int(flipped, 2)
+
+        # #better, time - O(logn), space - O(1)
+        # mask = 1
+        # while mask<=num:
+        #     mask = mask << 1
+        # mask = mask -1
+
+        # return mask ^ num
+
+        mask = (1 << num.bit_length()) - 1
+        return num ^ mask
