d608eb91bd54e06b5fa307fd64ef0dbd9a414f41
Nikz1476 <nikhil.a1476@gmail.com>
2026-03-22 21:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (78.40%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        # time - O(logn), space - O(1)
+    
+        if dividend == -(2**31) and divisor == -1:
+            return 2**31 - 1
+    
+        sign = -1 if (dividend < 0) ^ (divisor < 0) else 1
+    
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+    
+        quotient = 0
+    
+        while dividend >= divisor:
+        
+            temp = divisor
+            multiple = 1
+        
+            while dividend >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+            
+            dividend -= temp
+            quotient += multiple
+        
+        return sign * quotient
+        
