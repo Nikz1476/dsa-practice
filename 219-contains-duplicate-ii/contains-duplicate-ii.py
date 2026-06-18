54dfbcb42f70592edb55d5d330970be57b87537c
Nikz1476 <nikhil.a1476@gmail.com>
2026-06-18 09:25:42 +0530

    Time: 27 ms (91.07%) | Memory: 23.7 MB (95.27%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,30 @@
+#from collections import defaultdict
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        # n = len(nums)
+
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         if nums[i] == nums[j] and abs(i - j) <= k:
+        #             return True
+
+        # return False
+
+        last_seen = {}
+
+        for i, num in enumerate(nums):
+
+            if num in last_seen:
+                if i - last_seen[num] <= k:
+                    return True
+
+            last_seen[num] = i
+
+        return False
+        
+        
