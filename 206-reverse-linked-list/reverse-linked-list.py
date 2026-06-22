cfdb55ff0e917d9912d04e77bbe2c5131738edec
Nikz1476 <nikhil.a1476@gmail.com>
2026-06-22 21:56:29 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (7.24%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,33 +5,66 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # brute force - time and space O(n)
-        # values = []
-        # curr = head
+        # values = []           O(n),O(n)
 
+        # curr = head
         # while curr:
         #     values.append(curr.val)
         #     curr = curr.next
 
-        # dummy = ListNode(0)
-        # curr = dummy
-
+        # curr = head
         # for val in reversed(values):
-        #     curr.next = ListNode(val)
+        #     curr.val = val
         #     curr = curr.next
 
-        # return dummy.next
+        # return head
+        
+        
+        
+        # better using stack  - O(n),O(n)
+        # if not head:
+        #     return None
 
-        # Optimal - inplace 
-        # time - O(n)
-        # space - O(1)
-        prev = None
-        curr = head
+        # stack = []
+        # curr = head
 
-        while curr:
-            next_node = curr.next   # store next
-            curr.next = prev        # reverse pointer
-            prev = curr             # move prev forward
-            curr = next_node        # move curr forward
+        # while curr:
+        #     stack.append(curr)
+        #     curr = curr.next
 
-        return prev
+        # new_head = stack.pop()
+        # curr = new_head
+
+        # while stack:
+        #     curr.next = stack.pop()
+        #     curr = curr.next
+
+        # curr.next = None
+
+        # return new_head
+
+        
+        #Optimal - iterative        O(n),O(1)
+        # prev = None
+        # curr = head
+
+        # while curr:
+        #     nxt = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = nxt
+
+        # return prev
+
+
+
+        #Optimal recursive - O(n),O(n)
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+
+        head.next.next = head
+        head.next = None
+
+        return new_head
