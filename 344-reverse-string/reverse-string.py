23d9e9a899a11b441d75133bdc0d08ee32f5db0e
Nikz1476 <nikhil.a1476@gmail.com>
2026-06-30 20:44:49 +0530

    Time: 2 ms (51.67%) | Memory: 23.6 MB (18.29%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,10 +3,34 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        #two pointers
-        # time - O(n), space - O(1)
-        left = 0
-        right = len(s) - 1
+        #recursion - O(n),O(n)
+        # def helper(left, right):
+        #     if left >= right:
+        #         return
+
+        #     s[left], s[right] = s[right], s[left]
+        #     helper(left + 1, right - 1)
+
+        # helper(0, len(s) - 1)
+
+        #stack - O(n),O(n)
+        # stack = []
+
+        # for ch in s:
+        #     stack.append(ch)
+
+        # for i in range(len(s)):
+        #     s[i] = stack.pop()
+
+        #built in python function - O(n),O(1)
+        # s.reverse()
+
+        #slicing - not in place - O(n),O(n)
+        # s[:] = s[::-1]
+
+
+        #optimal - O(n), O(1)
+        left, right = 0, len(s) - 1
 
         while left < right:
             s[left], s[right] = s[right], s[left]
