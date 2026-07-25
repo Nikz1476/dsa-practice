e413e9440991e064c8733c5c56661059c7f867c0
Nikz1476 <nikhil.a1476@gmail.com>
2026-07-25 14:38:07 +0530

    Time: 3 ms (16.25%) | Memory: 19.4 MB (14.36%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -0,0 +1,6 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        freq = {}
+        for i in range(len(sentence)):
+            freq[sentence[i]] = freq.get(sentence[i],0)+1
+        return len(freq) == 26
